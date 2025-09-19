b5f5060ec72a17f6082980866eb5387ec2fdd1f7
israel boluwatife <israelboluwatife196@gmail.com>
2025-09-19 16:58:49 +0100

    fix : update pizza image

--- src/data/PizzaData.js
+++ src/data/PizzaData.js
@@ -1,4 +1,13 @@
-import image1 from "../images/cheesse burger.jpg";
+import image1 from "../images/Margherita.webp";
+import image2 from "../images/Pepperoni.jpg";
+import image3 from "../images/BBQ Chicken.jpg";
+import image4 from "../images/Veggie Supreme.jpg";
+import image5 from "../images/Hawaiian-pizza.jpg";
+import image6 from "../images/Cheesy Delight.jpg";
+import image7 from "../images/Chicken Tikka.jpg";
+import image8 from "../images/Meat Feast.jpg";
+import image9 from "../images/Spinach & Feta.jpg";
+import image10 from "../images/Mexican Heat.jpg";
 
 const pizzaData = [
   {
@@ -13,7 +22,7 @@ const pizzaData = [
     id: 2,
     name: "Pepperoni",
     price: 14.99,
-    image: image1,
+    image: image2,
     desc: "Loaded with spicy pepperoni and cheese.",
     link: "/Pepperoni",
   },
@@ -21,7 +30,7 @@ const pizzaData = [
     id: 3,
     name: "BBQ Chicken",
     price: 15.99,
-    image: image1,
+    image: image3,
     desc: "Grilled chicken with smoky BBQ sauce and onions.",
     link: "/BBQChicken",
   },
@@ -29,7 +38,7 @@ const pizzaData = [
     id: 4,
     name: "Veggie Delight",
     price: 13.49,
-    image: image1,
+    image: image4,
     desc: "Fresh veggies with tomato sauce and cheese.",
     link: "/Veggie Delight",
   },
@@ -37,23 +46,23 @@ const pizzaData = [
     id: 5,
     name: "Hawaiian",
     price: 14.49,
-    image: image1,
+    image: image5,
     desc: "Tropical mix of pineapple and ham on cheese.",
     link: "/Hawaiian",
   },
   {
     id: 6,
-    name: "Cheese Burst",
+    name: "Cheesy Delight",
     price: 13.99,
-    image: image1,
-    desc: "Oozing cheese from the inside and out.",
+    image: image6,
+    desc: "Loaded with 3 types of cheese for extra indulgence.",
     link: "/Cheese Burst",
   },
   {
     id: 7,
     name: "Tandoori Paneer",
     price: 14.79,
-    image: image1,
+    image: image7,
     desc: "Indian-style paneer with spicy tandoori flavors.",
     link: "/Tandoori Paneer",
   },
@@ -61,7 +70,7 @@ const pizzaData = [
     id: 8,
     name: "Meat Lovers",
     price: 16.99,
-    image: image1,
+    image: image8,
     desc: "A feast of bacon, beef, ham, and sausage.",
     link: "/Meat Lovers",
   },
@@ -69,7 +78,7 @@ const pizzaData = [
     id: 9,
     name: "Spinach Feta",
     price: 13.89,
-    image: image1,
+    image: image9,
     desc: "Creamy feta with fresh spinach and garlic.",
     link: "/Spinach Feta",
   },
@@ -77,7 +86,7 @@ const pizzaData = [
     id: 10,
     name: "Mexican Wave",
     price: 15.49,
-    image: image1,
+    image: image10,
     desc: "Mexican spices, jalapeños, corn, and cheese.",
     link: "/Mexican Wave",
   },
